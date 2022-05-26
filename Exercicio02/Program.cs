38a1c14ff7f44a7c3a2829b31a57bea45c73fb3d
JonathasGomes <jonathas553@hotmail.com>
2022-05-26 19:15:06 -0300

    Exercicio02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math.Sqrt = Raiz quadrada
+            //Math.Pow = Elevado
+
+            double Base, Altura, Area, Perimetro, Diagonal;
+
+            Base = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Area = Base * Altura;
+            Perimetro = 2 * Base + 2 * Altura;
+            Diagonal = Math.Sqrt(Math.Pow(Base, 2.0) + Math.Pow(Altura, 2.0));
+
+            Console.WriteLine("Area = " + Area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + Perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + Diagonal.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ bee1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace bee1005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Media;
+            float A, B;
+            A = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Media = (A + B) / 2;
+
+            Console.WriteLine("MEDIA = " + Media.ToString("F5"), CultureInfo.InvariantCulture);
+        }
+    }
+}
