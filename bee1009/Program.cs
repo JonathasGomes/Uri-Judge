722a1187a83bdbf82191932e02741f3bbe3d3ef4
JonathasGomes <jonathas553@hotmail.com>
2022-05-27 14:24:18 -0300

    Exercicio03, bee1009, bee1011

---
+++ bee1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace bee1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salariofixo, vendas, total;
+
+            nome = Console.ReadLine();
+            salariofixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            total = salariofixo + (vendas * 15.0 / 100);
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ bee1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace bee1011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, PI, Volume;
+            PI = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Volume = (4.0 / 3) * PI * Math.Pow(R, 3.0);
+
+            Console.WriteLine("VOLUME = " + Volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
