82360ce739a5a0ec0ef7eff3a839992f6873e353
JonathasGomes <jonathas553@hotmail.com>
2022-08-23 13:14:10 -0300

    DoWhile01

---
+++ bee1096/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bee1096
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+
+            for (int i = 1; i < 10; i++)
+            {
+                j = 7;
+                Console.Write("I=" + i);
+                Console.WriteLine(" J=" + j);
+                j--;
+                Console.Write("I=" + i);
+                Console.WriteLine(" J=" + j);
+                j--;
+                Console.Write("I=" + i);
+                Console.WriteLine(" J=" + j);
+            }
+        }
+    }
+}
---
+++ bee1116/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace bee1116
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, y, div;
+            string[] vet;
+
+
+            n = int.Parse(Console.ReadLine());
+            div = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                vet = Console.ReadLine().Split(' ');
+
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+                
+                if(x == 0 || y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    div = x / y;
+                    Console.WriteLine(div);
+                }
+
+
+            }
+        }
+    }
+}
