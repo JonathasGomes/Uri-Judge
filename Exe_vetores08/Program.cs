3db61a205c0694c2c001f3e0aed7b63bf40bfb2b
JonathasGomes <jonathas553@hotmail.com>
2022-11-06 11:38:48 -0300

    vetores09

---
+++ Exe_vetores08/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_vetores08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, countM, countF;
+            double menor, maior, soma, media;
+            double[] alturas;
+            string[] s, sexo;
+
+            N = int.Parse(Console.ReadLine());
+
+            alturas = new double[N];
+            sexo = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+
+                alturas[i] = double.Parse(s[0], CultureInfo.InvariantCulture);
+                sexo[i] = s[1];
+            }
+
+            maior = 0;
+            menor = 99.99;
+
+            for(int i = 0; i < N; i++)
+            {
+                if(alturas[i] > maior)
+                {
+                    maior = alturas[i];
+                }
+
+                if(alturas[i] < menor)
+                {
+                    menor = alturas[i];
+                }
+            }
+
+            countF = 0;
+            countM = 0;
+            soma = 0.0;
+            media = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if(sexo[i] == "M")
+                {
+                    countM++;
+                }
+
+                if(sexo[i] == "F")
+                {
+                    soma = soma + alturas[i];
+                    countF++;
+                }
+            }
+
+            media = soma / countF;
+            Console.WriteLine("Menor altura = " + menor);
+            Console.WriteLine("Maior altura = " + maior);
+            Console.WriteLine("Media das alturas das mulheres = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Numero dos homens = " + countM);
+        }
+
+    }
+}
---
+++ Exe_vetores09/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_vetores09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            string[] s, nome;
+            double[] preçovenda, preçocompra;
+            double lucro, totalvenda, totalcompra, porcentagemLucro;
+
+            N = int.Parse(Console.ReadLine());
+
+            nome = new string[N];
+            preçovenda = new double[N];
+            preçocompra = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+
+                nome[i] = s[0];
+                preçovenda[i] = double.Parse(s[1], CultureInfo.InvariantCulture);
+                preçocompra[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            lucro = 0.0;
+            totalcompra = 0.0;
+            totalvenda = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+                lucro = lucro + (preçocompra[i] - preçovenda[i]);
+                totalcompra = totalcompra + preçocompra[i];
+                totalvenda = totalvenda + preçovenda[i];
+            }
+
+            int countAbaixode10 = 0;
+            int countEntre10a20 = 0;
+            int countAcimade20 = 0;
+
+            porcentagemLucro = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+                porcentagemLucro = lucro / preçocompra[i] * 100;
+
+                if (porcentagemLucro < 10.0)
+                {
+                    countAbaixode10++;
+                }
+                else if (porcentagemLucro <= 20.0)
+                {
+                    countEntre10a20++;
+                }
+                else
+                {
+                    countAcimade20++;
+                }
+            }
+            Console.WriteLine("Lucro abaixo de 10%: " + countAbaixode10);
+            Console.WriteLine("Lucro entre 10% e 20%: " + countEntre10a20);
+            Console.WriteLine("Lucro acima de 20%: " + countAcimade20);
+            Console.WriteLine("Compra total: " + totalcompra.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Venda total: " + totalvenda.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Lucro total: " + lucro.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
